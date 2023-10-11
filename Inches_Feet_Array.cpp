5784117263d5261a8c216751fd1541dc0227542c
Harshit Mishra <hmharsh123@gmail.com>
2023-10-11 22:46:39 +0530

    Completed Inches_Feet_Array code

--- Inches_Feet_Array.cpp
+++ Inches_Feet_Array.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-class distance{
+class distances{
     private:
     float feet;
     float inches;
@@ -8,8 +8,25 @@ class distance{
     void input(){
         cout<<"Enter Feets:\n";
         cin>>feet;
+        cout<<"Enter Inches:\n";
+        cin>>inches;
     }
     void display(){
-
+        cout<<"Inches : "<<inches<<"Feet : "<<feet<<endl;
     }
 };
+int main(){
+    int n;
+    cout<<"Enter Size:\n";
+    cin>>n;
+    distances *arr = new distances[n];
+    for(int i=0;i<n;i++){
+        cout<<"For object"<<i+1<<endl;
+        arr[i].input();
+    }
+    for(int i=0;i<n;i++){
+        arr[i].display();
+    }
+    delete arr;
+    return 0; 
+}
