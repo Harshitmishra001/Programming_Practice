11eb57021d95f1b0ccedbfc6abb9776b342b5568
Harshit Mishra <hmharsh123@gmail.com>
2023-10-12 09:30:01 +0530

    Voting System program Started

---
+++ Voting_Candidates.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter Number of Participants: \n";
+    cin>>n;
+    int *arr =new int [n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        if(arr[i]){
+            
+        }
+    }
+    return 0;
+}
