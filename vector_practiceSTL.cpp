3de09fa3ad721bcdc6b05707603d27aae973f896
Harshit_Shadow <hmharsh123@gmail.com>
2024-06-14 23:33:26 +0530

    Vector STL

---
+++ vector_practiceSTL.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector <int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<endl;
+    }
+    cout<<v.capacity()<<endl;
+    return 0;
+    v.pop_back();
+}
+
